b231a415406892f12384ef44affa2e3c8793f72c
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2024-10-10 01:03:07 -0700

    ready for test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Adjust if serving from a subdirectory
+  build: {
+    outDir: "dist", // Ensure this matches your deployment directory
+  },
+});
